51c9f665c28fa7fae54bf2044ebad1b17964bf60
William Mata <wmata0141@gmail.com>
2019-05-09 22:09:10 -0400

    second commit: menu principal y rutas de los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './components/employee/employee-list/employee-list.component';
+import { EmployeeCreateComponent } from './components/employee/employee-create/employee-create.component';
+import { EmployeeEditComponent } from './components/employee/employee-edit/employee-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'employee-list', pathMatch: 'full' },
+  { path: 'employee-list', component: EmployeeListComponent, },
+  { path: 'employee-create', component: EmployeeCreateComponent, },
+  { path: 'employee-edit/:id', component: EmployeeEditComponent, },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

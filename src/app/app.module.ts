6ec6ef616b13fd2cd4353c6870b911987b05297d
William Mata <wmata0141@gmail.com>
2019-05-09 21:01:49 -0400

    first commit: estructura de carpetas crud employee, service, y model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './components/employee/employee-list/employee-list.component';
+import { EmployeeEditComponent } from './components/employee/employee-edit/employee-edit.component';
+import { EmployeeCreateComponent } from './components/employee/employee-create/employee-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeEditComponent,
+    EmployeeCreateComponent
   ],
   imports: [
     BrowserModule,
